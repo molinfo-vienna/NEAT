7935fec1d5c1b495e48814ded9b3bbbbb276a863
DanielRose95 <danielrose95@gmx.de>
2025-10-16 19:05:28 +0200

    implemented some of the GPT logic

--- src/molgen/model/decoder.py
+++ src/molgen/model/decoder.py
@@ -8,128 +8,12 @@ https://github.com/huggingface/transformers/blob/main/src/transformers/models/gp
 """
 
 import math
-import inspect
 
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-
-class LayerNorm(nn.Module):
-    """LayerNorm but with an optional bias. PyTorch doesn't support simply bias=False"""
-
-    def __init__(self, ndim, bias):
-        super().__init__()
-        self.weight = nn.Parameter(torch.ones(ndim))
-        self.bias = nn.Parameter(torch.zeros(ndim)) if bias else None
-
-    def forward(self, input):
-        return F.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)
-
-
-class CausalSelfAttention(nn.Module):
-
-    def __init__(self, config):
-        super().__init__()
-        assert config.n_embd % config.n_head == 0
-        # key, query, value projections for all heads, but in a batch
-        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
-        # output projection
-        self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
-        # regularization
-        self.attn_dropout = nn.Dropout(config.dropout)
-        self.resid_dropout = nn.Dropout(config.dropout)
-        self.n_head = config.n_head
-        self.n_embd = config.n_embd
-        self.dropout = config.dropout
-        # flash attention make GPU go brrrrr but support is only in PyTorch >= 2.0
-        self.flash = hasattr(torch.nn.functional, "scaled_dot_product_attention")
-        if not self.flash:
-            print(
-                "WARNING: using slow attention. Flash Attention requires PyTorch >= 2.0"
-            )
-            # causal mask to ensure that attention is only applied to the left in the input sequence
-            self.register_buffer(
-                "bias",
-                torch.tril(torch.ones(config.block_size, config.block_size)).view(
-                    1, 1, config.block_size, config.block_size
-                ),
-            )
-
-    def forward(self, x):
-        B, T, C = (
-            x.size()
-        )  # batch size, sequence length, embedding dimensionality (n_embd)
-
-        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-        q, k, v = self.c_attn(x).split(self.n_embd, dim=2)
-        k = k.view(B, T, self.n_head, C // self.n_head).transpose(
-            1, 2
-        )  # (B, nh, T, hs)
-        q = q.view(B, T, self.n_head, C // self.n_head).transpose(
-            1, 2
-        )  # (B, nh, T, hs)
-        v = v.view(B, T, self.n_head, C // self.n_head).transpose(
-            1, 2
-        )  # (B, nh, T, hs)
-
-        # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
-        if self.flash:
-            # efficient attention using Flash Attention CUDA kernels
-            y = torch.nn.functional.scaled_dot_product_attention(
-                q,
-                k,
-                v,
-                attn_mask=None,
-                dropout_p=self.dropout if self.training else 0,
-                is_causal=True,
-            )
-        else:
-            # manual implementation of attention
-            att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-            att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float("-inf"))
-            att = F.softmax(att, dim=-1)
-            att = self.attn_dropout(att)
-            y = att @ v  # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-        y = (
-            y.transpose(1, 2).contiguous().view(B, T, C)
-        )  # re-assemble all head outputs side by side
-
-        # output projection
-        y = self.resid_dropout(self.c_proj(y))
-        return y
-
-
-class MLP(nn.Module):
-
-    def __init__(self, config):
-        super().__init__()
-        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
-        self.gelu = nn.GELU()
-        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias)
-        self.dropout = nn.Dropout(config.dropout)
-
-    def forward(self, x):
-        x = self.c_fc(x)
-        x = self.gelu(x)
-        x = self.c_proj(x)
-        x = self.dropout(x)
-        return x
-
-
-class Block(nn.Module):
-
-    def __init__(self, config):
-        super().__init__()
-        self.ln_1 = LayerNorm(config.n_embd, bias=config.bias)
-        self.attn = CausalSelfAttention(config)
-        self.ln_2 = LayerNorm(config.n_embd, bias=config.bias)
-        self.mlp = MLP(config)
-
-    def forward(self, x):
-        x = x + self.attn(self.ln_1(x))
-        x = x + self.mlp(self.ln_2(x))
-        return x
+from .modules import LayerNorm, MLP, Block
 
 
 class Decoder(nn.Module):
@@ -159,7 +43,12 @@ class Decoder(nn.Module):
                 wte=nn.Embedding(self.vocab_size, self.n_embd),
                 wpe=nn.Embedding(self.block_size, self.n_embd),
                 drop=nn.Dropout(self.dropout),
-                h=nn.ModuleList([Block(self.config) for _ in range(self.n_layer)]),
+                h=nn.ModuleList(
+                    [
+                        Block(self.n_embd, self.n_head, self.dropout, self.bias)
+                        for _ in range(self.n_layer)
+                    ]
+                ),
                 ln_f=LayerNorm(self.n_embd, bias=self.bias),
             )
         )
@@ -234,6 +123,8 @@ class Decoder(nn.Module):
             loss = None
 
         return logits, loss
+    
+    # --- These methods are from the original GPT code but are not currently used ---
 
     # def crop_block_size(self, block_size):
     #     # model surgery to decrease the block size if necessary
@@ -248,53 +139,21 @@ class Decoder(nn.Module):
     #         if hasattr(block.attn, "bias"):
     #             block.attn.bias = block.attn.bias[:, :, :block_size, :block_size]
 
-    def configure_optimizers(self, weight_decay, learning_rate, betas, device_type):
-        # start with all of the candidate parameters
-        param_dict = {pn: p for pn, p in self.named_parameters()}
-        # filter out those that do not require grad
-        param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
-        # create optim groups. Any parameters that is 2D will be weight decayed, otherwise no.
-        # i.e. all weight tensors in matmuls + embeddings decay, all biases and layernorms don't.
-        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
-        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
-        optim_groups = [
-            {"params": decay_params, "weight_decay": weight_decay},
-            {"params": nodecay_params, "weight_decay": 0.0},
-        ]
-        num_decay_params = sum(p.numel() for p in decay_params)
-        num_nodecay_params = sum(p.numel() for p in nodecay_params)
-        print(
-            f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters"
-        )
-        print(
-            f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters"
-        )
-        # Create AdamW optimizer and use the fused version if it is available
-        fused_available = "fused" in inspect.signature(torch.optim.AdamW).parameters
-        use_fused = fused_available and device_type == "cuda"
-        extra_args = dict(fused=True) if use_fused else dict()
-        optimizer = torch.optim.AdamW(
-            optim_groups, lr=learning_rate, betas=betas, **extra_args
-        )
-        print(f"using fused AdamW: {use_fused}")
-
-        return optimizer
-
-    def estimate_mfu(self, fwdbwd_per_iter, dt):
-        """estimate model flops utilization (MFU) in units of A100 bfloat16 peak FLOPS"""
-        # first estimate the number of flops we do per iteration.
-        # see PaLM paper Appendix B as ref: https://arxiv.org/abs/2204.02311
-        N = self.get_num_params()
-        cfg = self.config
-        L, H, Q, T = cfg.n_layer, cfg.n_head, cfg.n_embd // cfg.n_head, cfg.block_size
-        flops_per_token = 6 * N + 12 * L * H * Q * T
-        flops_per_fwdbwd = flops_per_token * T
-        flops_per_iter = flops_per_fwdbwd * fwdbwd_per_iter
-        # express our flops throughput as ratio of A100 bfloat16 peak flops
-        flops_achieved = flops_per_iter * (1.0 / dt)  # per second
-        flops_promised = 312e12  # A100 GPU bfloat16 peak flops is 312 TFLOPS
-        mfu = flops_achieved / flops_promised
-        return mfu
+    # def estimate_mfu(self, fwdbwd_per_iter, dt):
+    #     """estimate model flops utilization (MFU) in units of A100 bfloat16 peak FLOPS"""
+    #     # first estimate the number of flops we do per iteration.
+    #     # see PaLM paper Appendix B as ref: https://arxiv.org/abs/2204.02311
+    #     N = self.get_num_params()
+    #     cfg = self.config
+    #     L, H, Q, T = cfg.n_layer, cfg.n_head, cfg.n_embd // cfg.n_head, cfg.block_size
+    #     flops_per_token = 6 * N + 12 * L * H * Q * T
+    #     flops_per_fwdbwd = flops_per_token * T
+    #     flops_per_iter = flops_per_fwdbwd * fwdbwd_per_iter
+    #     # express our flops throughput as ratio of A100 bfloat16 peak flops
+    #     flops_achieved = flops_per_iter * (1.0 / dt)  # per second
+    #     flops_promised = 312e12  # A100 GPU bfloat16 peak flops is 312 TFLOPS
+    #     mfu = flops_achieved / flops_promised
+    #     return mfu
 
     # @torch.no_grad()
     # def generate(self, idx, max_new_tokens, temperature=1.0, top_k=None):
--- src/molgen/model/molgen.py
+++ src/molgen/model/molgen.py
@@ -1,12 +1,16 @@
 from typing import Union
+import inspect
+import math
 
 import torch
 from torch import Tensor
 from lightning import LightningModule
 from torch_geometric.data import Data
 from torch.optim import Optimizer, AdamW
+from torch.nn import functional as F
 
 from .decoder import Decoder
+from .modules import LayerNorm, MLP, Block
 
 
 class MolGen(LightningModule):
@@ -15,35 +19,166 @@ class MolGen(LightningModule):
         self.save_hyperparameters()
         # self.hparams.setdefault("key", "value")
 
-        # Define model layers here
-        self.decoder1 = torch.nn.Identity()
-        self.decoder2 = torch.nn.Identity()
+        # Atom type embedding layer
+        self.wte = torch.nn.Embedding(self.vocab_size, self.n_embd)
+
+        # Fourier features for embedding of Cartesian coordinates
+        self.fourier_features = torch.nn.Identity()
+
+        # A linear layer for projecting the Cartesian coordinates (additional to the Fourier features)
+        self.coord_proj = torch.nn.Identity()
+
+        # Positional embedding layer for sequences (only important for causal transformer)
+        self.wpe = torch.nn.Embedding(self.block_size, self.n_embd)
+
+        # Dropout layer
+        self.drop = torch.nn.Dropout(self.dropout)
+
+        # Transformer blocks
+        self.h = (
+            torch.nn.ModuleList(
+                [
+                    Block(self.n_embd, self.n_head, self.dropout, self.bias)
+                    for _ in range(self.n_layer)
+                ]
+            ),
+        )
+        self.ln_f = LayerNorm(self.n_embd, bias=self.bias)
+
+        # Linear layer to map the final embeddings to atom vocabulary logits
+        self.lm_head = torch.nn.Linear(self.n_embd, self.vocab_size, bias=False)
+
+        # The atom types are supervised with a cross-entropy loss
+        self.bce_loss = torch.nn.BCEWithLogitsLoss()
+
+        # Weight tying
+        # with weight tying when using torch.compile() some warnings get generated:
+        # "UserWarning: functional_call was passed multiple values for tied weights.
+        # This behavior is deprecated and will be an error in future versions"
+        # not 100% sure what this is, so far seems to be harmless. TODO investigate
+        self.wte.weight = self.lm_head.weight
+
+        # So far it is the GPT logic, here comes additional stuff
+
+        # A second transformer block
+        self.h2 = (
+            torch.nn.ModuleList(
+                [
+                    Block(self.n_embd, self.n_head, self.dropout, self.bias)
+                    for _ in range(self.n_layer)
+                ]
+            ),
+        )
+        self.ln_f2 = LayerNorm(self.n_embd, bias=self.bias)
+
+        # A simple MLP with layer norm used for the denoising step
+        self.flow_matching_mlp = torch.nn.Identity()
 
         # Define loss functions here
-        self.ce_loss = torch.nn.CrossEntropyLoss()
         # self.fm_loss = FlowMatchingLoss()
 
-    def forward(self, data: Data) -> Data:
-        token_prob = self.decoder1(data)
-        token = torch.argmax(token_prob, dim=-1)
-        data.x.append(token)
-        condition = self.decoder2(data)
-        position = self.fm_loss(condition, data)
-        data.pos.append(position)
-        return data
+        # init all weights
+        self.apply(self._init_weights)
+        # apply special scaled init to the residual projections, per GPT-2 paper
+        for pn, p in self.named_parameters():
+            if pn.endswith("c_proj.weight"):
+                torch.nn.init.normal_(
+                    p, mean=0.0, std=0.02 / math.sqrt(2 * self.n_layer)
+                )
+
+        # report number of parameters
+        print("number of parameters: %.2fM" % (self.get_num_params() / 1e6,))
+
+    def get_num_params(self, non_embedding=True):
+        """
+        Return the number of parameters in the model.
+        For non-embedding count (default), the position embeddings get subtracted.
+        The token embeddings would too, except due to the parameter sharing these
+        params are actually used as weights in the final layer, so we include them.
+        """
+        n_params = sum(p.numel() for p in self.parameters())
+        if non_embedding:
+            n_params -= self.wpe.weight.numel()
+        return n_params
+
+    def _init_weights(self, module):
+        if isinstance(module, torch.nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, torch.nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    def forward(self, data, targets=None):
+        device = data.x.device
+        batch_size, sequence_length = data.x.size()
+        assert (
+            sequence_length <= self.config.block_size
+        ), f"Cannot forward sequence of length {sequence_length}, block size is only {self.config.block_size}"
+        sequence_position = torch.arange(
+            0, sequence_length, dtype=torch.long, device=device
+        )  # shape (t)
+
+        # forward the GPT model itself
+        tok_emb = self.wte(data.x)  # token embeddings of shape (b, t, n_embd)
+        pos_emb = self.wpe(
+            sequence_position
+        )  # position embeddings of shape (t, n_embd)
+        x = self.drop(tok_emb + pos_emb)
+        for block in self.h:
+            x = block(x)
+        x = self.ln_f(x)
+
+        if targets is not None:
+            # if we are given some desired targets also calculate the loss
+            logits = self.lm_head(x)
+            loss = F.cross_entropy(
+                logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1
+            )
+        else:
+            # inference-time mini-optimization: only forward the lm_head on the very last position
+            logits = self.lm_head(
+                x[:, [-1], :]
+            )  # note: using list [-1] to preserve the time dim
+            loss = None
+
+        return logits, loss
 
     def data_augmentation(self, data: Data) -> Data:
         # Implement data augmentation logic here
         return data
 
-    def configure_optimizers(self) -> tuple[list[Optimizer], list[dict]]:
-        optimizer = AdamW(
-            self.parameters(),
-            lr=self.hparams.learning_rate,
-            weight_decay=self.hparams.weight_decay,
+    def configure_optimizers(self, weight_decay, learning_rate, betas, device_type):
+        # start with all of the candidate parameters
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        # filter out those that do not require grad
+        param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
+        # create optim groups. Any parameters that is 2D will be weight decayed, otherwise no.
+        # i.e. all weight tensors in matmuls + embeddings decay, all biases and layernorms don't.
+        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+        optim_groups = [
+            {"params": decay_params, "weight_decay": weight_decay},
+            {"params": nodecay_params, "weight_decay": 0.0},
+        ]
+        num_decay_params = sum(p.numel() for p in decay_params)
+        num_nodecay_params = sum(p.numel() for p in nodecay_params)
+        print(
+            f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters"
         )
+        print(
+            f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters"
+        )
+        # Create AdamW optimizer and use the fused version if it is available
+        fused_available = "fused" in inspect.signature(torch.optim.AdamW).parameters
+        use_fused = fused_available and device_type == "cuda"
+        extra_args = dict(fused=True) if use_fused else dict()
+        optimizer = torch.optim.AdamW(
+            optim_groups, lr=learning_rate, betas=betas, **extra_args
+        )
+        print(f"using fused AdamW: {use_fused}")
 
-        return [optimizer]
+        return optimizer
 
     def shared_step(self, batch: Data, batch_idx: int) -> Tensor:
         y_hat = self(batch)
