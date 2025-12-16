07b49e5ec48b73e7bd0016e055e48b23654fb68a
DanielRose95 <danielrose95@gmx.de>
2025-12-16 14:16:29 +0100

    some changes

--- src/neat/dataset/dataset_geom.py
+++ src/neat/dataset/dataset_geom.py
@@ -1,28 +1,116 @@
 import logging
-import re
 import os
-from typing import List, Optional, Tuple, Dict, Set
+from typing import Dict
 
 import networkx as nx
 import numpy as np
 import torch
 import yaml
-from rdkit import Chem, RDLogger
-from rdkit.Chem import rdDetermineBonds
+from rdkit import RDLogger
 from torch_geometric.data import Data, InMemoryDataset
 from tqdm import tqdm
 
 import random
 import pickle
 import subprocess
-import json
 
-from rdkit import Chem
-
+import os
+import pickle
+from tqdm import tqdm
+import os
+import pickle
+import json, urllib
 
 RDLogger.DisableLog("rdApp.*")
 
 SEED = 0
+
+
+def process_molecule(mol_dict, vocabulary, num_conformers=30):
+    try:
+        conformers_info = mol_dict.get("conformers", [])
+        if len(conformers_info) == 0:
+            logging.warning(
+                f"No conformers found for {mol_dict['smiles']}, skipping molecule."
+            )
+            return None
+
+        energies = np.array(
+            [conformer["totalenergy"] for conformer in conformers_info],
+            dtype=float,
+        )
+        # Sort conformers by energy and select the top `num_conformers`
+        # This is probably not necessary, since the conformers should
+        # already be sorted by energy, but just in case.
+        order = np.argsort(energies)
+        keep = order[:num_conformers]
+
+        data_list = []  # to store Data objects for each conformer
+
+        for i in keep:
+            conformer_info = conformers_info[i]
+            mol = conformer_info["rd_mol"]
+            if mol is None:
+                logging.warning(
+                    f"RDKit molecule is None for {mol_dict['smiles']}, conformer {i}, skipping conformer."
+                )
+                continue
+
+            # Create a tensor for atomic numbers and hybridization states
+            x = torch.tensor(
+                [vocabulary[atom.GetAtomicNum()] for atom in mol.GetAtoms()],
+                dtype=torch.long,
+            )
+
+            # 3D coordinates centered at origin
+            conf = mol.GetConformer()
+            n = mol.GetNumAtoms()
+            pos = torch.zeros((n, 3), dtype=torch.float32)
+            for i in range(n):
+                p = conf.GetAtomPosition(i)  # returns RDKit Point3D
+                pos[i, 0] = p.x
+                pos[i, 1] = p.y
+                pos[i, 2] = p.z
+            pos = pos - pos.mean(dim=0, keepdim=True)
+
+            # Bond information for data augmentation during training
+            # Note that the generation does not use bond information
+            edge_index = []
+            for bond in mol.GetBonds():
+                i = bond.GetBeginAtomIdx()
+                j = bond.GetEndAtomIdx()
+                edge_index.append((i, j))
+                edge_index.append((j, i))
+
+            edge_index = torch.tensor(edge_index, dtype=torch.long).t().contiguous()
+
+            # Initialize molecular graph
+            G = nx.Graph()
+            for i, j in edge_index.t().tolist():
+                G.add_edge(i, j)
+
+            # Compute eccentricities for all nodes
+            eccentricities = nx.eccentricity(G)
+            eccentricity_tensor = torch.tensor(
+                [eccentricities[node] for node in range(len(G.nodes))],
+                dtype=torch.long,
+            )
+
+            data = Data(
+                x=x,
+                pos=pos,
+                edge_index=edge_index,
+                eccentricity=eccentricity_tensor,
+                smiles=mol_dict["smiles"],
+            )
+
+            data_list.append(data)
+
+        return data_list
+
+    except Exception as e:
+        print(f"Error processing {mol_dict['smiles']}: {e}")
+        return None
 
 
 class GEOMDataSet(InMemoryDataset):
@@ -140,7 +228,7 @@ class GEOMDataSet(InMemoryDataset):
         )
         try:
             with open(vocab_path, "r") as file:
-                self.vocabulary = yaml.safe_load(file)
+                vocabulary = yaml.safe_load(file)
         except yaml.YAMLError as e:
             print(f"Error loading vocabulary YAML file: {e}")
 
@@ -163,20 +251,19 @@ class GEOMDataSet(InMemoryDataset):
             except Exception as e:
                 print(f"Error processing {smiles}: {e}")
 
+        # drug_path = os.path.join(self.root, "raw", "rdkit_folder", "drugs")
+
         data_list = []
         # Load all molecule pickles into self.mols
-        for i, pickle_path in tqdm(enumerate(pickle_paths)):
+        for pickle_path in tqdm(pickle_paths):
             try:
                 with open(pickle_path, "rb") as f:
                     mol_dict = pickle.load(f)
 
-                molecule_data_list = self.process_molecule(mol_dict)
+                molecule_data_list = process_molecule(mol_dict, vocabulary=vocabulary)
                 if molecule_data_list is not None:
                     data_list.extend(molecule_data_list)
 
-                if i == 10000:
-                    logging.info("Loaded 100 molecule pickles...")
-                    break
             except Exception as e:
                 print(f"Error loading pickle {pickle_path}: {e}")
                 continue
@@ -190,91 +277,6 @@ class GEOMDataSet(InMemoryDataset):
             data_list = [self.pre_transform(data) for data in data_list]
 
         self.save(data_list, self.processed_paths[0])
-
-    def process_molecule(self, mol_dict):
-        try:
-            conformers_info = mol_dict.get("conformers", [])
-            if len(conformers_info) == 0:
-                logging.warning(
-                    f"No conformers found for {mol_dict['smiles']}, skipping molecule."
-                )
-                return None
-
-            energies = np.array(
-                [conformer["totalenergy"] for conformer in conformers_info], dtype=float
-            )
-            # Sort conformers by energy and select the top `num_conformers`
-            # This is probably not necessary, since the conformers should
-            # already be sorted by energy, but just in case.
-            order = np.argsort(energies)
-            keep = order[: self.num_conformers]
-
-            data_list = []  # to store Data objects for each conformer
-
-            for i in keep:
-                conformer_info = conformers_info[i]
-                mol = conformer_info["rd_mol"]
-                if mol is None:
-                    logging.warning(
-                        f"RDKit molecule is None for {mol_dict['smiles']}, conformer {i}, skipping conformer."
-                    )
-                    continue
-
-                # Create a tensor for atomic numbers and hybridization states
-                x = torch.tensor(
-                    [self.vocabulary[atom.GetAtomicNum()] for atom in mol.GetAtoms()],
-                    dtype=torch.long,
-                )
-
-                # 3D coordinates centered at origin
-                conf = mol.GetConformer()
-                n = mol.GetNumAtoms()
-                pos = torch.zeros((n, 3), dtype=torch.float32)
-                for i in range(n):
-                    p = conf.GetAtomPosition(i)  # returns RDKit Point3D
-                    pos[i, 0] = p.x
-                    pos[i, 1] = p.y
-                    pos[i, 2] = p.z
-                pos = pos - pos.mean(dim=0, keepdim=True)
-
-                # Bond information for data augmentation during training
-                # Note that the generation does not use bond information
-                edge_index = []
-                for bond in mol.GetBonds():
-                    i = bond.GetBeginAtomIdx()
-                    j = bond.GetEndAtomIdx()
-                    edge_index.append((i, j))
-                    edge_index.append((j, i))
-
-                edge_index = torch.tensor(edge_index, dtype=torch.long).t().contiguous()
-
-                # Initialize molecular graph
-                G = nx.Graph()
-                for i, j in edge_index.t().tolist():
-                    G.add_edge(i, j)
-
-                # Compute eccentricities for all nodes
-                eccentricities = nx.eccentricity(G)
-                eccentricity_tensor = torch.tensor(
-                    [eccentricities[node] for node in range(len(G.nodes))],
-                    dtype=torch.long,
-                )
-
-                data = Data(
-                    x=x,
-                    pos=pos,
-                    edge_index=edge_index,
-                    eccentricity=eccentricity_tensor,
-                    smiles=mol_dict["smiles"],
-                )
-
-                data_list.append(data)
-
-            return data_list
-
-        except Exception as e:
-            print(f"Error processing {mol_dict['smiles']}: {e}")
-            return None
 
     def get_random_splits(
         self,
