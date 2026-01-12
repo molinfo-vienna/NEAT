implemented batched generation and fixed evaluation memory overflow

scripts/generation.py
@@ -5,6 +5,7 @@ import torch
 import torch_geometric
 import yaml
 from lightning import seed_everything
+from torch_geometric.data import Batch
 
 from neat.model import NEAT
 from neat.model.molecule_builder import MoleculeBuilder
@@ -48,40 +49,56 @@ def generate(args: argparse.Namespace) -> None:
     model = MODEL.load_from_checkpoint(CHECKPOINTS_PATH, map_location=device)
 
     seeds = [i for i in range(params.get("num_runs", 1))]
+
+    num_molecules = params["num_molecules"]
+    batch_size = params["batch_size"]
+    num_batches = (num_molecules + batch_size - 1) // batch_size
+    if (num_molecules % batch_size) == 0:
+        num_mols_per_batch = [batch_size] * num_batches
+    else:
+        num_mols_per_batch = [batch_size] * (num_batches - 1) + [
+            num_molecules % batch_size
+        ]
+
     for seed in seeds:
         torch_geometric.seed_everything(seed)
         seed_everything(seed)
 
-        with torch.no_grad():
-            model.eval()
-            if "prefix_path" in params:
-                builder = MoleculeBuilder()
-                prefix_x, prefix_pos, _ = builder.load_tensor_from_file(
-                    os.path.join(ROOT, params["prefix_path"])
-                )
-                prefix_pos -= prefix_pos.mean(dim=0, keepdim=True)
-                x, pos, batch = model.generate(
-                    batch_size=params["num_molecules"],
-                    max_atoms=params["max_atoms"],
-                    num_time_steps=params["num_time_steps"],
-                    prefix_x=prefix_x,
-                    prefix_pos=prefix_pos,
-                    time_step_spacing=params["time_step_spacing"],
-                )
-            else:
-                x, pos, batch = model.generate(
-                    batch_size=params["num_molecules"],
-                    max_atoms=params["max_atoms"],
-                    num_time_steps=params["num_time_steps"],
-                    time_step_spacing=params["time_step_spacing"],
-                )
+        generated_batches = []
+        for batch_idx in range(num_batches):
+            num_mols_batch = num_mols_per_batch[batch_idx]
+            with torch.no_grad():
+                model.eval()
+                if "prefix_path" in params:
+                    builder = MoleculeBuilder()
+                    prefix_x, prefix_pos, _ = builder.load_tensor_from_file(
+                        os.path.join(ROOT, params["prefix_path"])
+                    )
+                    prefix_pos -= prefix_pos.mean(dim=0, keepdim=True)
+                    generated_batch = model.generate(
+                        batch_size=num_mols_batch,
+                        max_atoms=params["max_atoms"],
+                        num_time_steps=params["num_time_steps"],
+                        prefix_x=prefix_x,
+                        prefix_pos=prefix_pos,
+                        time_step_spacing=params["time_step_spacing"],
+                    )
+                else:
+                    generated_batch = model.generate(
+                        batch_size=num_mols_batch,
+                        max_atoms=params["max_atoms"],
+                        num_time_steps=params["num_time_steps"],
+                        time_step_spacing=params["time_step_spacing"],
+                    )
+            generated_batches.append(generated_batch)
 
-        out_dir = os.path.join(ROOT, params["output_path"], "unconditional", f"seed_{seed}")
+        generated_mols = Batch.from_data_list(generated_batches)
+        out_dir = os.path.join(
+            ROOT, params["output_path"], "unconditional", f"seed_{seed}"
+        )
         if not os.path.exists(out_dir):
             os.makedirs(out_dir)
-        torch.save(x, os.path.join(out_dir, "x.pt"))
-        torch.save(pos, os.path.join(out_dir, "pos.pt"))
-        torch.save(batch, os.path.join(out_dir, "batch.pt"))
+        torch.save(generated_mols, os.path.join(out_dir, "generated_mols.pt"))
 
 
 def parseArgs() -> argparse.Namespace:

scripts/generation_prefix.py
@@ -3,11 +3,11 @@ import ast
 import os
 
 from lightning import seed_everything
-import numpy as np
 from rdkit import Chem
 import torch
 import torch_geometric
 import yaml
+from torch_geometric.data import Batch
 
 from neat.model import NEAT
 from neat.dataset import DataModule, GEOMDataSet
@@ -97,33 +97,47 @@ def generate(args: argparse.Namespace) -> None:
     MODEL = NEAT
     model = MODEL.load_from_checkpoint(CHECKPOINTS_PATH, map_location=device)
 
-    for mol_index, ((x, pos), dummy_idx) in enumerate(zip(mols, dummy_idxs)):
-        with torch.no_grad():
-            model.eval()
-            n_atoms = x.size(0)
-            mask = torch.ones(n_atoms, dtype=torch.bool)
-            if len(dummy_idx) > 0:
-                mask[dummy_idx] = False
-            prefix_x = x[mask]
-            prefix_pos = pos[mask]
-            prefix_pos -= prefix_pos.mean(dim=0, keepdim=True)
-            x, pos, batch = model.generate(
-                batch_size=params["num_molecules"],
-                max_atoms=params["max_atoms"],
-                num_time_steps=params["num_time_steps"],
-                prefix_x=prefix_x,
-                prefix_pos=prefix_pos,
-                time_step_spacing=params["time_step_spacing"],
-            )
+    num_molecules = params["num_molecules"]
+    batch_size = params["batch_size"]
+    num_batches = (num_molecules + batch_size - 1) // batch_size
+    if (num_molecules % batch_size) == 0:
+        num_mols_per_batch = [batch_size] * num_batches
+    else:
+        num_mols_per_batch = [batch_size] * (num_batches - 1) + [
+            num_molecules % batch_size
+        ]
 
-            out_dir = os.path.join(
-                params["output_path"], "prefix", f"prefix_{mol_index}"
-            )
-            if not os.path.exists(out_dir):
-                os.makedirs(out_dir)
-            torch.save(x, os.path.join(out_dir, "x.pt"))
-            torch.save(pos, os.path.join(out_dir, "pos.pt"))
-            torch.save(batch, os.path.join(out_dir, "batch.pt"))
+    for mol_index, ((x, pos), dummy_idx) in enumerate(zip(mols, dummy_idxs)):
+
+        generated_batches = []
+        for batch_idx in range(num_batches):
+            num_mols_batch = num_mols_per_batch[batch_idx]
+
+            with torch.no_grad():
+                model.eval()
+                n_atoms = x.size(0)
+                mask = torch.ones(n_atoms, dtype=torch.bool)
+                if len(dummy_idx) > 0:
+                    mask[dummy_idx] = False
+                prefix_x = x[mask]
+                prefix_pos = pos[mask]
+                prefix_pos -= prefix_pos.mean(dim=0, keepdim=True)
+                generated_batch = model.generate(
+                    batch_size=num_mols_batch,
+                    max_atoms=params["max_atoms"],
+                    num_time_steps=params["num_time_steps"],
+                    prefix_x=prefix_x,
+                    prefix_pos=prefix_pos,
+                    time_step_spacing=params["time_step_spacing"],
+                )
+            generated_batches.append(generated_batch)
+
+        generated_mols = Batch.from_data_list(generated_batches)
+
+        out_dir = os.path.join(params["output_path"], "prefix", f"prefix_{mol_index}")
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+        torch.save(generated_mols, os.path.join(out_dir, "generated_mols.pt"))
 
 
 def parseArgs() -> argparse.Namespace:

src/neat/model/molecule_builder.py
@@ -2,7 +2,7 @@ import logging
 import os
 
 import torch
-from rdkit.Chem import Mol, rdDetermineBonds, rdmolfiles
+from rdkit.Chem import Mol, rdDetermineBonds, rdmolfiles, MolToSmiles
 from tqdm import tqdm
 
 
@@ -53,10 +53,14 @@ class MoleculeBuilder:
         Returns:
             tuple[torch.Tensor, torch.Tensor, torch.Tensor]: A tuple containing the atom types, positions, and batch indices.
         """
-        x = torch.load(os.path.join(files_path, "x.pt")).detach().cpu()
-        pos = torch.load(os.path.join(files_path, "pos.pt")).detach().cpu()
-        batch = torch.load(os.path.join(files_path, "batch.pt")).detach().cpu()
-        return x, pos, batch
+        generated_mols = (
+            torch.load(
+                os.path.join(files_path, "generated_mols.pt"), weights_only=False
+            )
+            .detach()
+            .cpu()
+        )
+        return generated_mols.x, generated_mols.pos, generated_mols.batch
 
     def create_xyz_block(self, x: torch.Tensor, pos: torch.Tensor) -> str:
         """
@@ -86,6 +90,7 @@ class MoleculeBuilder:
         pos: torch.Tensor,
         batch: torch.Tensor,
         progress_bar: bool = False,
+        break_after_k_mols: int = None,
     ) -> list[Mol]:
         """
         Generates RDKit molecules from tensors of atom types and positions.
@@ -121,6 +126,56 @@ class MoleculeBuilder:
                     f"An error occurred while determining bonds for molecule in batch {batch_id}: {e}"
                 )
                 mol = None
+
             mols.append(mol)
 
+            if break_after_k_mols is not None and len(mols) >= break_after_k_mols:
+                break
+
         return mols
+
+    def generate_smiles_from_rdkit_molecules(
+        self,
+        x: torch.Tensor,
+        pos: torch.Tensor,
+        batch: torch.Tensor,
+        progress_bar: bool = False,
+    ) -> list[Mol]:
+        """
+        Generates RDKit molecules from tensors of atom types and positions.
+        Args:
+            x (torch.Tensor): A tensor of shape (n_atoms,).
+            pos (torch.Tensor): A tensor of shape (n_atoms, 3).
+            batch (torch.Tensor): A tensor of shape (n_atoms,).
+        Returns:
+            list[Mol]: A list of RDKit molecules.
+        """
+        smiles = []
+        unique_batches = batch.unique().tolist()
+
+        iterator = unique_batches
+        if progress_bar:
+            iterator = tqdm(unique_batches, desc="Generating RDKit molecules")
+        for batch_id in iterator:
+            mask = batch == batch_id
+            x_mol = x[mask]
+            pos_mol = pos[mask]
+
+            xyz_block = self.create_xyz_block(x_mol, pos_mol)
+            mol = rdmolfiles.MolFromXYZBlock(xyz_block)
+            try:
+                rdDetermineBonds.DetermineBonds(mol, charge=0, maxIterations=100000)
+            except ValueError:
+                mol = None
+            except Exception as e:
+                logging.warning(
+                    f"An error occurred while determining bonds for molecule in batch {batch_id}: {e}"
+                )
+                mol = None
+            if mol is not None:
+                smiles_str = MolToSmiles(mol, canonical=True)
+                smiles.append(smiles_str)
+            else:
+                smiles.append(None)
+
+        return smiles

src/neat/model/neat.py
@@ -14,6 +14,7 @@ from torch.nn import functional as F
 from torch.optim import Optimizer
 from torch_geometric.data import Data
 from torch_geometric.nn.pool import global_mean_pool
+from torch_geometric.data import Batch
 
 from .attention import Block
 from .positional_encoding import AxialRotaryPositionEncoding, FourierPositionEncoding
@@ -819,7 +820,7 @@ class NEAT(LightningModule):
                 pos -= pos.mean(dim=0, keepdim=True)
                 batch_source = torch.cat(updated_batch, dim=0)  # [batch_size]
 
-        return x, pos, batch_source
+        return Batch(x=x, pos=pos, batch=batch_source)
 
     def calculate_positions(
         self,